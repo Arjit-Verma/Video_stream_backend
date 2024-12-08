ba5bffe721c0b14548851fc403a74c0216ae643e
Arjit <arjitverma73@gmail.com>
2024-12-08 15:13:38 +0530

    change to check if it is working or not

--- index.js
+++ index.js
@@ -6,24 +6,34 @@ import connectDB from "./config/db.js";
 import videoRoutes from "./routes/videoRoutes.js";
 import path from "path";
 import { fileURLToPath } from "url";
+import fs from "fs";
+
 const app = express();
 
 connectDB();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-//using cors middleware for security and adding headers and other stuff
+// Serve static files (videos, HLS files, etc.)
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+
+// Route to serve the index.html file
 app.get("/watchIt", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
+
+// Middleware
 app.use(cors());
 app.use(express.json());
 app.use("/api/videos", videoRoutes);
+
+// Log all incoming requests
 app.use((req, res, next) => {
   console.log(`${req.method} ${req.path}`);
   next();
 });
 
+// Start the server
 app.listen(4000, "0.0.0.0", () => {
-  console.log("Sunn rha hu Port :4000");
+  console.log("Server running on port 4000");
 });
